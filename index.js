09893882751062eccdc102808558d48a792f3ffb
prkm93 <aradshub93@gmail.com>
2021-09-10 08:37:36 +0530

    updated emoji

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ const btn = document.querySelector('.btn-check');
 const output = document.querySelector('#output');
 
 btn.addEventListener('click', function(){
+
+    //grabbing date and number
     let dateValue = date.value;
     let numberValue = number.value;
 
-    console.log(dateValue.length);
-    console.log(numberValue.length);
+    // splitting date into numbers for sum
     const splitted = dateValue.split("-")
     const sum = Number(splitted[0]) + Number(splitted[1]) + Number(splitted[2]);
     
+    // validations
     if(!(numberValue.length && dateValue.length)) {
         output.innerHTML = 'Please enter both fields'
     } else if (sum % (+numberValue) === 0) {
